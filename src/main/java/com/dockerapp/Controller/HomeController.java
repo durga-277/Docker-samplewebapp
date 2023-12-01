f5742c6591ba3c39c7e0b4afacc1158fdfca8bb3
durga-277 <152302913+durga-277@users.noreply.github.com>
2023-12-01 18:39:52 +0530

    initial commit

---
+++ src/main/java/com/dockerapp/Controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.dockerapp.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping("/home")
+    public String welcomeHome(){
+        return "HEllo welcome to Doceker-app";
+    }
+    @GetMapping("/get")
+    public String sayHello(){
+        return "Hi dude,how are you";
+    }
+
+}
